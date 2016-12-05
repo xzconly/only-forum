ee1f581ab3dc643caeee66e03a9405c6fefd1d57
only <xingzctouch@163.com>
2016-12-05 14:16:22 +0800

    add upload avatar func

--- routes/user.py
+++ routes/user.py
@@ -84,3 +84,38 @@ def update_password(model_id):
 def profile():
     user = current_user()
     return render_template('/user/profile.html', user=user)
+
+
+@main.route('/upload', methods=['POST'])
+def upload_file():
+    import json
+    import config
+    # 通过 request.files 访问上传的文件
+    # uploaded 是上传时候的文件名
+    f = request.files.get('uploaded')
+    if f:
+        filename = f.filename
+        user = current_user()
+        filename = str(user.id) + '.' + filename.split('.')[-1]
+        path = config.uploads_avatar_dir + filename
+        f.save(path)
+        msg_dict = dict(
+            msg='头像上传成功',
+            status=1,
+            filename = filename,
+            img_url = url_for('.uploaded_file', filename=filename),
+        )
+    else:
+        msg_dict = dict(
+            msg='头像上传失败',
+            status=0,
+        )
+    return json.dumps(msg_dict)
+
+
+@main.route('/uploads/<filename>')
+def uploaded_file(filename):
+    # 用这个函数直接返回文件的响应
+    import config
+    return flask.send_from_directory(config.uploads_avatar_dir, filename)
+
